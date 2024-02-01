7fefcd5b933e7f7e73e624aa5e7061f5ac8d4202
Mohsin Khan <mkhan6720@gmail.com>
2024-02-01 17:37:37 +0530

    Contract complete

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.7.6",
   networks: {
     sepolia: {
-      url: import.meta.env.VITE_SEPOLIA_URL,
-      account: [import.meta.env.VITE_PRIVATE_KEY],
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,28 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SingleSwap = await hre.ethers.getContractFactory("SingleSwap");
+  const singleSwap = await SingleSwap.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await singleSwap.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Contract deployed at ${singleSwap.target}`);
+  // 0xb448B43d843883D016d100087B24264FE3FDA874
 }
 
 // We recommend this pattern to be able to use async/await everywhere
